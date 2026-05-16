3d1f6a3e7d67213a96e2406dd94f0635901f7efd
joelfergus <jaynexus12@gmail.com>
2026-05-16 08:37:16 +0100

    operators added

---
+++ operators/index.py
@@ -0,0 +1,39 @@
+# arithmetic operators
+num1 = 7 #assigning a value to num1
+num2 = 13
+# addition
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+# subtraction
+difference = num1 - num2
+print("The difference between", num1, "and", num2, "is", difference)
+# multiplication
+product = num1 * num2
+print("The product of", num1, "and", num2, "is", product)
+# division 
+quotient = num2 / num1
+print("The quotient of", num2, "divided by", num1, "is", quotient)
+# modulo (operator that gives the remainder of a division)
+remainder = num2 % num1
+print("The remainder when", num2, "is divided by", num1, "is", remainder)
+# exponentiation (power operator)
+exponentiation = num1 ** num2
+print("The result of", num1, "raised to the power of", num2, "is", exponentiation)
+
+
+
+# comparison operators
+a = 10
+b = 20
+# equal to
+print("Is a equal to b?", a == b)
+# not equal to
+print("Is a not equal to b?", a != b)
+# less than
+print("Is a less than b?", a < b)
+# greater than
+print("Is a greater than b?", a > b)
+# less than or equal to
+print("Is a less than or equal to b?", a <= b)
+# greater than or equal to
+print("Is a greater than or equal to b?", a >= b)
